bb2aa5713ab88cf8b4ca9482f6d040a4ea4c78a5
Rowa <jrowah@gmail.com>
2022-04-20 02:39:56 +0300

    Solved chapter 3 and 2

--- Chapter3/index.js
+++ Chapter3/index.js
@@ -15,3 +15,71 @@ function isEven(n) {
     else if (n < 0) return isEven(-n);
     else return isEven(n - 2);
   }
+
+//   Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters there are in the string.
+function countBs(str){
+    let numBs = 0
+    for(let i=0; i<str.length; i++){
+      if(str[i] === 'B'){
+        numBs += 1
+      } 
+    } return numBs
+  } 
+
+//Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+function countChar(str, char){
+    let numChar = 0
+    for(let i=0; i<str.length; i++){
+      if(str[i] === char){
+        numChar += 1
+      } 
+    } return numChar
+  } 
+
+  //Looping a triangle
+//   Write a loop that makes seven calls to console.log to output the following triangle:
+//  #
+//  ##
+//  ###
+//  ####
+//  #####
+//  ######
+//  #######
+function makeTriangle(height){
+    for(let line = '#'; line.length <= height; line += '#') {
+        console.log(line)
+    }
+}
+
+makeTriangle(7)
+
+
+//FizzBuzz
+// Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+// When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+for(let i= 1; i<=100; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz')
+  } else if(i % 3 === 0){
+    console.log('Fizz')
+  } else if(i % 5 === 0) {
+    console.log('Buzz')
+  } else console.log(i)
+  }
+
+  //ChessBoard
+
+// Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+// // Passing this string to console.log should show something like this:
+//  # # # # 
+// # # # #
+//  # # # # 
+// # # # #
+//  # # # # 
+// # # # #
+//  # # # # 
+// # # # #
+
+
+// When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid
+// of the given width and height.
